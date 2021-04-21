faa9d8a677ea8dea50d6c71a8b10abe4fb65d63e
NetitoRCs <77469863+NetitoRCs@users.noreply.github.com>
2021-04-21 12:24:17 -0500

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.EnvConfig={"env_production":true,"fb_app_id":677505792353827,"gplus_client_id":"686981379285-oroivr8u2ag1dtm3ntcs6vi05i3cpv0j.apps.googleusercontent.com","master_url":"m.agar.io","socketEndpoint":'wss://web-live-v2-0.agario.miniclippt.com/ws',"xsolla_endpoint":"https://web.archive.org/web/20151209015838/https://payments.agario.miniclippt.com","fb_endpoint":"https://web.archive.org/web/20151209015838/https://rewardcallback-live.agario.miniclippt.com/payments/facebook","game_url":"https://web.archive.org/web/20151209015838/http://agar.io"}
+
+}
+/*
+     FILE ARCHIVED ON 01:58:38 Dec 09, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:09:13 Apr 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  esindex: 0.016
+  PetaboxLoader3.datanode: 2704.433 (5)
+  LoadShardBlock: 175.299 (3)
+  exclusion.robots.policy: 0.184
+  exclusion.robots: 0.195
+  load_resource: 2750.042 (2)
+  PetaboxLoader3.resolve: 134.537 (2)
+  captures_list: 223.099
+  RedisCDXSource: 5.909
+  CDXLines.iter: 33.073 (3)
+*/
---
+++ i18n.js
@@ -0,0 +1,39 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+i18n_lang='en';i18n_dict={"en":{"advertisement":"Advertisement","back_button":"Back","changelog":"Changelog","connect_help":"If you cannot connect to the servers, check if you have some anti virus or firewall blocking the connection.","connecting":"Connecting","continue":"Continue","create_party":"Create","create_party_instructions":"Give this link to your friends:","creating_party":"Creating party...","fb_matchresults_description":"Can you do better? Come and play!","fb_matchresults_subtitle":"I survived %@m %@s and got a score of %@","fb_matchresults_title":"Got a new score in Agar.io","g_plus_share_stats":"I survived %@m %@s and got a score of %@ in Agar.io! Can you do better? Come and play!","gamemode_experimental":"Experimental","gamemode_ffa":"FFA","gamemode_teams":"Teams","games_played":"Games played","highest_mass":"Highest mass","instructions_mouse":"Move your mouse to control your cell","instructions_space":"Press <b>Space</b> to split","instructions_w":"Press <b>W</b> to eject some mass","join_party":"Join","join_party_instructions":"Your friend should have given you a code, type it here:","joined_party_instructions":"You are now playing with this party:","joining_party":"Joining party...","last_match_results":"Last match results","leaderboard":"Leaderboard","leaderboard_time":"Leaderboard Time","login_and_play":"Login and play","login_tooltip":"Login to get your benefits!","logout":"Logout","longest_survival":"Longest survival","mass_eaten":"Mass Eaten","menu_login_facebook":"Sign in with Facebook","menu_login_google":"Sign in with Google","option_dark_theme":"Dark theme","option_no_colors":"No colors","option_no_names":"No names","option_no_skins":"No skins","option_show_mass":"Show mass","option_skip_stats":"Skip stats","party":"Party","party_description":"Play with your friends in the same map","party_join_error":"There was a problem joining that party, please make sure the code is correct, or try creating another party","play":"Play","play_as_guest":"Play as guest","player_cells_eaten":"Player Cells Eaten","position_1":"First","position_10":"Tenth","position_2":"Second","position_3":"Third","position_4":"Fourth","position_5":"Fifth","position_6":"Sixth","position_7":"Seventh","position_8":"Eighth","position_9":"Ninth","privacy_policy":"Privacy Policy","region_australia":"Australia","region_china":"China","region_east_asia":"East Asia","region_europe":"Europe","region_north_america":"North America","region_oceania":"Oceania","region_players":"players","region_poland":"Poland","region_russia":"Russia","region_select":" -- Select a Region -- ","region_south_america":"South America","region_turkey":"Turkey","region_us_east":"US East","region_us_west":"US West","score":"Score","share":"Share","shop":"Shop","spectate":"Spectate","stats":"Stats","stats_cells_eaten":"cells eaten","stats_food_eaten":"food eaten","stats_highest_mass":"highest mass","stats_leaderboard_time":"leaderboard time","stats_time_alive":"time alive","stats_top_position":"top position","survival_time":"Survival Time","terms_of_service":"Terms of Service","top_position":"Top Position","total_cells_eaten":"Total cells eaten","total_mass_eaten":"Total mass eaten","unnamed_cell":"An unnamed cell"}};i18n_lang=(window.navigator.userLanguage||window.navigator.language||'en').split('-')[0];if(!i18n_dict.hasOwnProperty(i18n_lang))i18n_lang='en';i18n=i18n_dict[i18n_lang];function parseString(tag,token,arr){var finalText=i18n[tag];for(i=0;i<arr.length;i++)
+{finalText=finalText.replace(token,arr[i]);}
+return finalText;}
+window["parseString"]=parseString;
+
+}
+/*
+     FILE ARCHIVED ON 01:58:38 Dec 09, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:09:10 Apr 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 224.997
+  exclusion.robots: 0.112
+  exclusion.robots.policy: 0.102
+  RedisCDXSource: 2.631
+  esindex: 0.03
+  LoadShardBlock: 201.738 (3)
+  PetaboxLoader3.datanode: 237.737 (5)
+  CDXLines.iter: 16.338 (3)
+  load_resource: 150.203 (2)
+  PetaboxLoader3.resolve: 54.829 (2)
+*/
